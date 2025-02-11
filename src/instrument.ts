c85ee7072d4cd35b552a943122a0d3d4514739a7
dev-737 <73829355+dev-737@users.noreply.github.com>
2025-02-11 11:08:31 +0530

    chore(deps): update deps to latest

--- src/instrument.ts
+++ src/instrument.ts
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import Constants from '#utils/Constants.js';
 import 'dotenv/config';
 
@@ -7,11 +8,14 @@ if (!Constants.isDevBuild) {
     dsn: process.env.SENTRY_DSN,
     release: `interchat@${Constants.ProjectVersion}`,
     tracesSampleRate: 1.0,
+    profilesSampleRate: 1.0,
     maxValueLength: 1000,
     integrations: [
+      Sentry.captureConsoleIntegration(),
       Sentry.onUncaughtExceptionIntegration({
         exitEvenIfOtherHandlersAreRegistered: false,
       }),
+      nodeProfilingIntegration(),
     ],
   });
 }
