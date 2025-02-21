d70191a3ae975aa3ce64d7cff90ce294f875dc07
dev-737 <73829355+dev-737@users.noreply.github.com>
2025-02-21 14:42:53 +0530

    feat: chinese language support

--- src/commands/Main/set/language.ts
+++ src/commands/Main/set/language.ts
@@ -28,7 +28,7 @@ import {
   ApplicationCommandOptionType,
 } from 'discord.js';
 
-const currSupportedLangs = ['en', 'hi', 'es', 'pt'] as const;
+const currSupportedLangs = ['en', 'hi', 'es', 'pt', 'zh'] as const;
 
 export default class SetLanguage extends BaseCommand {
   constructor() {
